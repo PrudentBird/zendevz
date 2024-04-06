ac4c9f3b1bea119c77491dcf8ee7c7cfb216feca
PrudentBird <prudentbird@gmail.com>
2024-04-06 08:16:13 +0100

    Version History

--- src/components/Subhero/Subhero.jsx
+++ src/components/Subhero/Subhero.jsx
@@ -14,11 +14,10 @@ const Subhero = () => {
             <h3>Social Media Management</h3>
             <div className="overlay">
               <div className="overlay-text">
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Explicabo, saepe illo expedita reiciendis
-                consectetur corporis, perferendis sapiente minus asperiores
-                sequi dolor laborum eaque iusto nihil quae error?
-                Necessitatibus, dicta non.
+                Maximize Your Social Media Impact with Zen Developers. We
+                deliver customized social media management designed to increase
+                brand awareness, generate leads, and elevate your online
+                presence.
               </div>
             </div>
             <div className="contimg-wrap">
@@ -33,11 +32,11 @@ const Subhero = () => {
             </h3>
             <div className="overlay">
               <div className="overlay-text">
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Explicabo, saepe illo expedita reiciendis
-                consectetur corporis, perferendis sapiente minus asperiores
-                sequi dolor laborum eaque iusto nihil quae error?
-                Necessitatibus, dicta non.
+                Zen Developers' comprehensive SEO services will optimize your
+                website for search engine success. Our experts conduct in-depth
+                keyword research, enhance technical SEO, build high-quality
+                backlinks, and craft compelling content to drive visibility and
+                organic traffic.
               </div>
             </div>
             <div className="conteimg-wrap">
@@ -54,11 +53,11 @@ const Subhero = () => {
             </h3>
             <div className="overlay">
               <div className="overlay-text">
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Explicabo, saepe illo expedita reiciendis
-                consectetur corporis, perferendis sapiente minus asperiores
-                sequi dolor laborum eaque iusto nihil quae error?
-                Necessitatibus, dicta non.
+                Zen Developers understands that your business is unique. We'll
+                work closely with you to understand your target audience,
+                business goals, and competitive landscape. This allows us to
+                tailor a digital marketing plan that's custom-designed to
+                deliver maximum results and a strong return on investment.
               </div>
             </div>
             <div className="contenimg-wrap">
@@ -73,11 +72,11 @@ const Subhero = () => {
             </h3>
             <div className="overlay">
               <div className="overlay-text">
-                Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Explicabo, saepe illo expedita reiciendis
-                consectetur corporis, perferendis sapiente minus asperiores
-                sequi dolor laborum eaque iusto nihil quae error?
-                Necessitatibus, dicta non.
+                Zen Developers partners with you to craft a website that
+                perfectly aligns with your brand and business objectives.
+                Whether you need a lead-generating website, a robust e-commerce
+                platform, or a custom web application, our team delivers
+                tailored solutions that empower your online success.
               </div>
             </div>
             <div className="contentimg-wrap">
