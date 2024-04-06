7b77fb02b756dd53c7675d9d4d18b26739284d14
PrudentBird <prudentbird@gmail.com>
2024-04-06 09:01:20 +0100

    Version History

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../components/Contact/Contact.css";
 import Nav from "../components/Nav/Nav";
 import Footer from "../components/Footer/Footer";
+import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
+  const formRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('Form Submitted');
-  }
+
+    emailjs
+      .sendForm("service_us6s24p", "template_qppp4jx", formRef.current, {
+        publicKey: "4G1zuCH7_ES82eJJ0",
+      })
+      .then(
+        () => {
+          console.log("Form Submitted!");
+          toast.success("Form Submitted!");
+        },
+        (error) => {
+          console.log("Something Went Wrong!...", error.text);
+          toast.error("Something Went Wrong!");
+        }
+      );
+  };
 
   return (
     <div className="contact-wrapper">
@@ -19,24 +37,44 @@ const Contact = () => {
           <p>Fill the form below to send us a message</p>
         </div>
         <div className="contact-form-wrap">
-          <form method="post" action="/" className="contact-form">
+          <form method="post" className="contact-form" ref={formRef}>
             <div className="contact-form-input-wrap">
               <label htmlFor="name">Name</label>
-              <input type="text" id="name" name="name" required placeholder="John Doe"/>
+              <input
+                type="text"
+                id="name"
+                name="user_name"
+                required
+                placeholder="John Doe"
+              />
             </div>
             <div className="contact-form-input-wrap">
               <label htmlFor="email">Email</label>
-              <input type="email" id="email" name="email" required placeholder="johndoe@gmail.com" />
+              <input
+                type="email"
+                id="email"
+                name="user_email"
+                required
+                placeholder="johndoe@gmail.com"
+              />
             </div>
             <div className="contact-form-input-wrap">
               <label htmlFor="message">Message</label>
-              <input id="message" name="message" required placeholder="Your message here" />
+              <input
+                id="message"
+                name="message"
+                required
+                placeholder="Your message here"
+              />
             </div>
-            <button type="button" onClick={handleSubmit}>Send Message</button>
+            <button type="button" onClick={handleSubmit}>
+              Send Message
+            </button>
           </form>
         </div>
       </div>
       <Footer />
+      <ToastContainer />
     </div>
   );
 };
